a32d4bcaf32d9cfdc3193f6173f3c91f776ab4b7
ALEKSEY <andrsmek@gmail.com>
2025-07-23 16:57:55 +0300

    api routes fix2

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import App from "./pages/_app"
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error('Root element not found');
